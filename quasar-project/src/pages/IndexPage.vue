50d988ad06bb5ccf63bf074d099461e3b99c8cb2
seulki-k <skbb11@naver.com>
2025-01-07 17:40:26 +0900

    a

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -2,7 +2,24 @@
   <div>
   <div style="height:90vh">
     <q-carousel animated v-model="slide" arrows navigation infinite style="height: 100%">
-      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+      <!-- :name 은 q-carousel-slide에서 사용하는 속성으로 slide의 고유 이름 부여 여기서는 1로 부터 -->
+      <q-carousel-slide :name="1"  img-src="https://cdn.quasar.dev/img/quasar.jpg" @click.left.shift="slide =2">
+        <div align="center">
+          <br />
+          <input v-model="newUser.name" placeholder="name" @keydown.prevent="handleKeyDown"/><br />
+          <input v-model="newUser.email" placeholder="email" />
+          <br /><br />
+          <hr />
+          <q-btn color="primary" label="유저 등록" @click="axiosApiAdd" />
+        </div>
+      </q-carousel-slide>
+
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" align="center" @click.left.shift="slide =3">
+        <!-- 단일속성 값이 아닌 객체에는 약어 사용 불가능 -->
+        <Qtable v-bind="propsData" @deleteData="deleteUser" />
+      </q-carousel-slide>
+
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/mountains.jpg" @click.left.shift="slide =1">
         <table border="1px" align="center" style="background-color: white; width: 90%">
           <thead>
             <tr>
@@ -21,21 +38,6 @@
         </table>
         <br />
       </q-carousel-slide>
-
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" align="center">
-        <!-- 단일속성 값이 아닌 객체에는 약어 사용 불가능 -->
-        <Qtable v-bind="propsData" @deleteData="deleteUser" />
-      </q-carousel-slide>
-      <q-carousel-slide :name="3"  img-src="https://cdn.quasar.dev/img/quasar.jpg">
-        <div align="center">
-          <br />
-          <input v-model="newUser.name" placeholder="name" /><br />
-          <input v-model="newUser.email" placeholder="email" />
-          <br /><br />
-          <hr />
-          <q-btn color="primary" label="유저 등록" @click="axiosApiAdd" />
-        </div>
-      </q-carousel-slide>
     </q-carousel>
   </div>
   <div align="center">
@@ -44,11 +46,10 @@
 </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { ref } from 'vue'
 import { api } from 'src/boot/axios'
 import Qtable from 'src/components/QCustomTable.vue'
-
 
 const deleteUser = (res) => {
   // 받은 데이터의 id 배열 설정
@@ -61,15 +62,32 @@ const deleteUser = (res) => {
       .delete(`/users/${id}`)
       .then((response) => {
         console.log(`ID ${id} 제거?=>`, response.data)
+        axiosApi()
       })
       .catch((err) => {
         console.error(`ID ${id} 삭제 실패: `, err)
       });
   });
 }
+
+
 const newUser = ref([])
 
 const data = ref([])
+
+const axiosApi = async() => {
+  await api
+    .get('/users')
+    .then((response) => {
+      data.value = response.data
+      console.log("res.data", response.data)
+      console.log(data.value)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
 const slide = ref(1)
 
 
@@ -102,16 +120,11 @@ const propsData = ref({
   pagination,
 })
 
-const axiosApi = () => {
-  api
-    .get('/users')
-    .then((response) => {
-      data.value = response.data
-      console.log("res.data", response.data)
-      console.log(data.value)
-    })
-    .catch((err) => {
-      console.error(err)
-    })
+
+const handleKeyDown = (e) =>{
+  if(e.key >= '0' && e.key <= '9'){
+    alert('이름에 숫자는 입력 불가능합니다.')
+    newUser.value.name = ''
+  }
 }
 </script>
